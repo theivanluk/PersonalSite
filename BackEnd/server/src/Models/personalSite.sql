36222733e72843fa454f76b940d91604c8cbce29
Ivan Luk <luk.ivan.mech@gmail.com>
2023-02-06 02:02:51 -0800

    Restructured to better fit MVC model

---
+++ BackEnd/server/src/Models/personalSite.sql
@@ -0,0 +1,46 @@
+DROP DATABASE IF EXISTS personalsite;
+
+CREATE DATABASE personalsite;
+
+\c personalsite;
+
+CREATE TABLE blog_post (
+  post_id SERIAL PRIMARY KEY,
+  post_title VARCHAR(200) NOT NULL,
+  picture VARCHAR(200) NOT NULL,
+  blog TEXT NOT NULL,
+  date_posted TIMESTAMPTZ NOT NULL
+);
+
+CREATE TABLE projects (
+  project_id SERIAL PRIMARY KEY,
+  project_name VARCHAR(50) NOT NULL,
+  project_description TEXT NOT NULL,
+  project_url VARCHAR(200) NOT NULL,
+  project_updated TIMESTAMPTZ NOT NULL,
+  project_pic VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE about_me (
+  description text NOT NULL,
+  lastupdated TIMESTAMPTZ NOT NULL,
+  displaypic VARCHAR(200) NOT NULL
+);
+
+CREATE TABLE contact_info (
+  id SERIAL,
+  name VARCHAR(50) NOT NULL,
+  displaypic VARCHAR(200) NOT NULL,
+  phone VARCHAR(50) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  linkedin VARCHAR(100) NOT NULL,
+  github VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE user_info (
+  user_id SERIAL PRIMARY KEY,
+  username VARCHAR(50) UNIQUE NOT NULL,
+  password VARCHAR(50) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  date_joined TIMESTAMPTZ NOT NULL
+);
---
+++ BackEnd/server/src/Models/queryModel.ts
@@ -0,0 +1,97 @@
+import { BlogModel, ProjectsModel, AboutMeModel, ContactInfoModel, UserInfoModel} from './DatabaseTypes'
+
+export const insert = {
+  to: {
+    Blog: (blogInput: BlogModel): string => {
+      const { postTitle, picture, blog, date_posted } = blogInput;
+      return `
+        INSERT INTO
+          blog_post (post_title, picture, blog, date_posted)
+        VALUES
+          ('${postTitle}','${picture}','${blog}','${date_posted}')
+      `
+    },
+    Projects: (projectInput: ProjectsModel): string => {
+      const { project_name, project_description, project_url, project_updated, project_pic} = projectInput;
+      return `
+        INSERT INTO
+          projects (project_name, project_description, project_url, project_updated, project_pic)
+        VALUES
+          ('${project_name}', '${project_description}', '${project_url}', '${project_updated}', '${project_pic}')
+      `
+    },
+    AboutMe: (aboutMeInput: AboutMeModel): string => {
+      const {description, lastupdated, displaypic} = aboutMeInput;
+      return `
+        INSERT INTO
+          about_me (description, lastupdated, displaypic)
+        VALUES
+          ('${description}', '${lastupdated}', '${displaypic}')
+      `
+    },
+    ContactInfo: (contactMeInput: ContactInfoModel): string => {
+      const { name, displaypic, phone, email, linkedin, github } = contactMeInput;
+      return `
+        INSERT INTO
+          contact_info (name, displaypic, phone, email, linkedin, github)
+        VALUES
+          ('${name}', '${displaypic}', '${phone}', '${email}','${linkedin}','${github}')
+      `
+    },
+    UserInfo: (userInfoInput: UserInfoModel): string => {
+      const { username, password, email, date_joined} = userInfoInput;
+      return `
+        INSERT INTO
+          user_info (username, password, email, date_joined)
+        VALUES
+          ('${username}', '${password}', '${email}', '${date_joined}')
+      `
+    }
+  }
+}
+
+export const getAll = {
+  by: {
+    mostRecent: {
+      Blog: (pages: number = 1, limit: number = 5): string => `
+        SELECT
+          *
+        FROM
+          blog_post
+        ORDER BY
+          date_posted DESC
+        LIMIT ${limit} OFFSET ${limit * (pages - 1)}
+        `,
+      Projects: (pages: number = 1, limit: number = 5): string => `
+        SELECT
+          *
+        FROM
+          blog_post
+        ORDER BY
+          date_posted DESC
+        LIMIT ${limit} OFFSET ${limit * (pages - 1)}
+        `,
+      AboutMe: (): string => `
+        SELECT
+          *
+        FROM
+          about_me
+        `,
+      ContactInfo: (): string => `
+        SELECT
+          *
+        FROM
+          contact_info
+        `,
+      UserInfo: (pages: number = 1, limit: number = 5): string => `
+        SELECT
+          *
+        FROM
+          user_info
+        ORDER BY
+          username ASC
+        LIMIT ${limit} OFFSET ${limit * (pages - 1)}
+        `,
+    }
+  }
+}
