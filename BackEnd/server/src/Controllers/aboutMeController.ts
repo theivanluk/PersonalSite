9722afe217cad39ffd8dab93b1e33cddd491849f
Ivan Luk <luk.ivan.mech@gmail.com>
2023-02-15 17:27:20 -0800

    Completed AboutMe Controllers

--- BackEnd/server/src/Controllers/aboutMeController.ts
+++ BackEnd/server/src/Controllers/aboutMeController.ts
@@ -1,6 +1,9 @@
+import { Request, Response } from 'express';
+
 import IDataAccess from '@/DataAccess/iDataAccess';
 import IAboutMeController from '@/Entities/ControllerEntities/iAboutMeController';
-import { Request, Response } from 'express';
+import { AboutMeModel, AboutMeFields, allAboutMeFields } from '@/Entities/DatabaseTypes';
+import { ValidationError, QueryError, handleControllerError } from '@/Entities/ErrorEntities';
 
 export default class AboutMeCntroller implements IAboutMeController {
   private dataAccess: IDataAccess;
@@ -9,13 +12,57 @@ export default class AboutMeCntroller implements IAboutMeController {
     this.dataAccess = dataAccess
   }
 
-  private validateAboutMeModel () {
-
+  private validateAboutMeModel(input: any): input is AboutMeModel {
+    return (
+      (input as AboutMeModel).description === 'string' &&
+      (input as AboutMeModel).displaypic === 'string'
+    );
   }
 
-  private validateAboutMeFields() {
-
+  private validateAboutMeFields(input: any): input is AboutMeFields {
+    return allAboutMeFields.includes(input as AboutMeFields);
   }
 
+  public async getAboutMe(req: Request, res: Response): Promise<void> {
+    try {
+      const data = <AboutMeModel[]> await this.dataAccess.getAboutMe();
+      if (data.length === 0) throw new QueryError();
+      res.status(200).json(data);
+    } catch(err) {
+      handleControllerError(err, res);
+    }
+  }
 
+  public async insertAboutMe(req: Request, res: Response): Promise<void> {
+    try {
+      if (!this.validateAboutMeModel(req.body)) throw new ValidationError();
+      // Validate database size;
+      await this.dataAccess.insertAboutMe(req.body);
+      res.sendStatus(201);
+    } catch(err) {
+      handleControllerError(err, res);
+    }
+  }
+
+  public async updateAboutMe(req: Request, res: Response): Promise<void> {
+    try {
+      const { field, data } = req.body;
+      if (
+        !this.validateAboutMeFields(field) &&
+        typeof data !== 'string'
+      ) throw new ValidationError();
+      await this.dataAccess.updateAboutMe(field, data);
+      res.sendStatus(200);
+    } catch(err) {
+      handleControllerError(err, res);
+    }
+  }
+
+  public async deleteAboutMe(req: Request, res: Response): Promise<void> {
+    try {
+      res.sendStatus(401);
+    } catch(err) {
+      handleControllerError(err, res);
+    }
+  }
 }
--- BackEnd/server/src/Entities/ControllerEntities/iAboutMeController.d.ts
+++ BackEnd/server/src/Entities/ControllerEntities/iAboutMeController.d.ts
@@ -1,3 +1,10 @@
-export default interface IAboutMeController {
+import IDataAccess from "@/DataAccess/iDataAccess"
+import { Request, Response } from "express";
 
+
+export default interface IAboutMeController {
+  async getAboutMe(req: Request, res: Response): Promise<void>;
+  async insertAboutMe(req: Request, res: Response): Promise<void>;
+  async updateAboutMe(req: Request, res: Response): Promise<void>;
+  async deleteAboutMe(req: Request, res: Response): Promise<void>;
 }
