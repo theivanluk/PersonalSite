6cd014c241e4f996a669763f58203046a2b67742
Ivan Luk <luk.ivan.mech@gmail.com>
2023-04-23 14:25:17 -0700

    Sanity update

---
+++ Sanity_Backend/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'byevj975',
+    dataset: 'production'
+  }
+})
---
+++ Sanity_Backend/schemas/index.ts
@@ -0,0 +1,13 @@
+// import { createSchema } from "sanity"
+// import { SchemaType } from "sanity"
+
+import testimonials from "./testimonials"
+import abouts from "./abouts";
+import skills from "./skills";
+import contact from "./contact";
+import brands from "./brands";
+import experiences from "./experiences";
+import workExperience from "./workExperience";
+import works from "./works";
+
+export const schemaTypes = [abouts, brands, contact, experiences, skills, testimonials, workExperience, works];
